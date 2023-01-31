32d47086a9377aa2283ebbc7491eac65f7592a08
Pato <98058202+Patoinks@users.noreply.github.com>
2023-01-31 17:31:42 +0000

    App Full Functionable

--- app/src/main/java/ipca/grupo2/UserDetailFragment.kt
+++ app/src/main/java/ipca/grupo2/UserDetailFragment.kt
@@ -5,6 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.core.os.bundleOf
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import ipca.grupo2.room.AppDatabase
 
 class UserDetailFragment : Fragment() {
 
@@ -15,7 +20,13 @@ class UserDetailFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_user_detail, container, false)
 
+        var curEventoId = context?.let { AppDatabase.getDatabase(it)?.eventoDao()?.getCurEventId()?.id }
 
+        view.findViewById<ImageView>(R.id.voltarEventoDet).setOnClickListener {
+            val bundle = bundleOf("eventoid" to curEventoId)
+            var navController: NavController? = null
+            findNavController().navigate(R.id.action_userDetailFragment_to_eventoDetalheFragment, bundle)
+        }
 
 
         return view
